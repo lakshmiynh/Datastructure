d1601a4da1b0a1397f511bd4fe51402805f31ce1
Lakshmi <lakshmiynh8@gmail.com>
2024-04-02 18:32:10 +0530

    Added:main method call all method of stack

---
+++ Runstack.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stackimplementation
+{
+     class Runstack
+    {
+        static void Main(string[] args)
+        {
+            NodeStack<int> Stack = new NodeStack<int>();
+            Stack.Push(1);
+            Stack.Push(2);
+            Stack.Push(3);
+            Stack.Push(4);
+            Stack.Display();
+            Console.WriteLine("top element");
+           Stack.Peek();
+            Console.WriteLine("after pop ");
+            Stack.Pop();
+            Stack.Display();
+            
+        }
+    }
+}

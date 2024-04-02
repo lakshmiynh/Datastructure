9a32cc29b284e8c4b4faf0ec959f9aa1e4f1094f
lakshmiynh <119153376+lakshmiynh@users.noreply.github.com>
2024-04-02 19:24:17 +0530

    queue implementation using linkedlist

---
+++ Nodequeue.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queueimplementation
+{
+    public class Nodequeue<T>
+    {
+        private Node head;
+        private Node tail;
+
+        public void Enqueue(T element)
+        {
+            var temp = new Node(element);
+
+            if(head == null)
+            {
+                head = tail = temp;
+            }
+            else
+            {
+               tail.Next = temp;
+              tail = temp;  
+            }
+        }
+
+        public T Dequeue()
+        {
+            if(head== null)
+            {
+                Console.WriteLine("queue empty");
+            }
+            var temp = head.Element;
+            head=(Node)head.Next;
+            return temp;
+        }
+
+        public void Peek()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Queue is empty");
+                //return default(T); // or throw an exception
+            }
+
+             Console.WriteLine(head.Element);
+        }
+
+        public void Display()
+        {
+            Node current = head;
+            while (current != null)
+            {
+                Console.WriteLine(current.Element);
+                current = current.Next;
+            }
+        }
+
+
+        private class Node
+        {
+            public Node(T element)
+            {
+                this.Element = element;
+                this.Next = null; // Initialize Next to null for the last node in the queue
+            }
+
+            public T Element { get; set; }
+            public Node Next { get; set; }
+        }
+
+    }
+}
---
+++ Runqueue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queueimplementation
+{
+    public  class Runqueue
+    {
+        static void Main(string[] args)
+        {
+            Nodequeue<int> queue = new Nodequeue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Display();
+            Console.WriteLine("after Dequeue");
+            queue.Dequeue();
+            queue.Display();
+            Console.WriteLine("first element in queue");
+            queue.Peek();
+        }
+        
+        
+          
+    }
+}
